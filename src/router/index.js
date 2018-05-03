f63287ab3517006c77c0d68b258d6165e67c88d7
fancy-cat <wangyafen121@126.com>
2018-05-03 14:11:42 +0800

    组件初始化完成

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,65 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
+//引入商品组件
+import Goods from '@/components/goods/list'
+import GoodsDetail from '@/components/goods/detail'
+
+//引入新闻组件
+import News from '@/components/news/list'
+import NewsDetail from '@/components/news/detail'
+
+//引入图片组件
+import Pictures from '@/components/pictures/list'
+import PicDetail from '@/components/pictures/detail'
+
+//引入底部tabbar组件
+import Cart from '@/components/tabbar/cart'
+import Home from '@/components/tabbar/home'
+import Member from '@/components/tabbar/member'
+import Search from '@/components/tabbar/search'
+
 export default new Router({
   routes: [
-  
+      {
+        path:'/cart',
+        component:Cart
+      },
+      {
+        path:'/home',
+        component:Home
+      },
+      {
+        path:'/search',
+        component:Search
+      },
+      {
+        path:'/member',
+        component:Member
+      },
+      {
+        path:'/goods',
+        component:Goods
+      },
+      {
+        path:'/goods/detail',
+        component:GoodsDetail
+      },
+      {
+        path:'/news',
+        component:News
+      },
+      {
+        path:'/news/detail',
+        component:NewsDetail
+      },
+      {
+        path:'/pics',
+        component:Pictures
+      },
+      {
+        path:'/pics/detail',
+        component:PicDetail
+      }
   ]
 })
