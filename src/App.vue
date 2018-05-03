320fe541adf171ad2241d5d8e632398363be3f5e
fancy-cat <wangyafen121@126.com>
2018-05-03 13:16:23 +0800

    项目初始化完成

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <div>这是头部</div>
+    <router-view/>
+    <div>这是底部</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
